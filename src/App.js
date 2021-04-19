5b0f4fcc04f6a3976d4aac8105684accddfce2b3
vino753 <69960684+vino753@users.noreply.github.com>
2021-04-18 23:23:04 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+
+import Table1 from './Table'
+import SimpleTabs from './Authentication'
+
+function App() {
+  return (
+    
+    <div className="App">
+    
+     <SimpleTabs/>
+      <BrowserRouter>
+          <Switch>
+            
+          </Switch>
+        </BrowserRouter>   
+        
+       
+    </div>
+  );
+} 
+
+export default App;
+
+
